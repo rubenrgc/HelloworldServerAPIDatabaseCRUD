7903902513c176863d660f447b695a1685c17d9d
rubenrgc <rubenrgc@hotmail.com>
2020-11-14 10:19:45 -0800

    segundo COmmit

--- src/index.js
+++ src/index.js
@@ -1,21 +1,66 @@
-const express = require('express');
-
-const app = express();
+const express = require("express");
+const { ApolloServer, gql } = require("apollo-server-express");
+// Run the server on a port specified in our .env file or port 4000
 const port = process.env.PORT || 4000;
 
-app.get('/',(req,res)=>res.send('Hello ruben!!!'));
+let notes = [
+  { id: "1", content: "This is a note", author: "Adam Scott" },
+  { id: "2", content: "This is another note", author: "Harlow Everly" },
+  { id: "3", content: "Oh hey look, another note!", author: "Riley Harrison" },
+];
+
+// Construct a schema, using GraphQL's schema language
+//aqui ponemos nuestros esquemas de datos de graphql
+const typeDefs = gql`
+  type Note {
+    id: ID!
+    content: String!
+    author: String!
+  }
+
+  type Query {
+    hello: String!
+    notes: [Note!]!
+    note(id: ID!): Note!
+  }
+  type Mutation {
+    newNote(content: String!): Note!
+  }
+`;
+// Provide resolver functions for our schema fields
+// aqui ponemos los resolvers (query and mutations)(consultas o mutaciones)
+const resolvers = {
+  Query: {
+    hello: () => "Hello world!",
+    notes: () => notes,
+    note: (parent, args) => {
+      return notes.find((note) => note.id === args.id);
+    },
+  },
+  Mutation: {
+    newNote: (parent, args) => {
+      let noteValue = {
+        id: String(notes.length + 1),
+        content: args.content,
+        author: "Adam Scott",
+      };
+      notes.push(noteValue);
+      return noteValue;
+    },
+  },
+};
+
+const app = express();
 
 
-app.listen(port, () =>
-console.log(`Server running at http://localhost:${port}`)
+// Apollo Server setup
+const server = new ApolloServer({ typeDefs, resolvers });
+
+// Apply the Apollo GraphQL middleware and set the path to /api
+server.applyMiddleware({ app, path: "/api" });
+
+app.listen({ port }, () =>
+  console.log(
+    `GraphQL Server running at http://localhost:${port}${server.graphqlPath}`
+  )
 );
-
-
-/*
-en este ejemplo, primero requeriremos la dependencia de exprpess
-y crearemos un objeto de la app, usando el modulo inportado de express.
-Despues usaremoslos objetos app el metodo get para instruir a nuestra 
-aplicadcion para enviar una respuesta de helloworld cuando un usuario acceda
-a el root URL (/). despues instruiremos nuestra plicacion  para correr
-en el puerto 4000.
-*/
